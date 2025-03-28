0b1a5c8635227c74cbed95a18bfe2c48eaa873e0
rdmochowski <rdmochowski@apple.com>
2025-03-27 19:01:41 -0700

    add template from starter spring for LLM sumnmary app

---
+++ LogLLMSummary/src/main/java/dev/rsd/LogLLMSummary/LogLlmSummaryApplication.java
@@ -0,0 +1,13 @@
+package dev.rsd.LogLLMSummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogLlmSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogLlmSummaryApplication.class, args);
+	}
+
+}
